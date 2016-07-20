3dca1a73e9b9c7015359ec86b4a24b230aa60a33
ASteinheiser <asteinhe@asu.edu>
2016-07-20 15:17:41 -0700

    update getadevice

--- exercises/get_a_device/solution/solution.js
+++ exercises/get_a_device/solution/solution.js
@@ -1,13 +1,9 @@
 var meshbluHttp = require('meshblu-http')
 
 var device = new meshbluHttp()
-var uuid, token
 
 device.register({type: "awesome-device"}, function(error, response) {
-  uuid = response.uuid
-  token = response.token
-
-  var newDevice = new meshbluHttp({uuid: uuid, token: token})
+  var newDevice = new meshbluHttp({uuid: response.uuid, token: response.token})
 
   newDevice.whoami(function(error, response) {
     console.log(response.type)
